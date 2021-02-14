3c8ecf62fd3039c319a64612f3d7581ae9a54e4c
DONDJ <artem.alex.alekseev@mail.ru>
2021-02-14 17:37:59 +0300

    move application logic to models.py as models methods

--- UrlShortenerApp/models.py
+++ UrlShortenerApp/models.py
@@ -1,8 +1,12 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import redirect
 from UserApp.models import User
 from PetProject.settings import SITE_BASE_URL
 import string
 import random
+from datetime import date, timedelta
 
 
 class ShortenedUrl(models.Model):
@@ -15,10 +19,17 @@ class ShortenedUrl(models.Model):
     class Meta:
         ordering = ['-pk']
 
-    def save(self, user, original_url):
-        self.user_creator = user
-        self.original_url = original_url
-        self.new_short_url = ShortenedUrl.get_shortened_url()
+    def save(self, user=None, original_url=None):
+        if not user or not original_url:  # обновление записи, не создание новой
+            self.user_creator = self.user_creator
+            self.original_url = self.original_url
+        else:  # создание новой записи
+            if isinstance(user, User):
+                self.user_creator = user
+            else:
+                self.user_creator = User.objects.get(pk=1)
+            self.original_url = original_url
+            self.new_short_url = ShortenedUrl.get_shortened_url()
         super(ShortenedUrl, self).save()
 
     @staticmethod
@@ -32,8 +43,54 @@ class ShortenedUrl(models.Model):
             if not new_url_existence:
                 return SITE_BASE_URL + generated_suffix
 
+    @staticmethod
+    def get_full_url_from_requested(request):
+        request_url = request.get_full_path()
+        final_url = SITE_BASE_URL + request_url[1:]
+        try:
+            original_URL_fromDB = ShortenedUrl.objects.get(new_short_url=final_url)
+            return original_URL_fromDB
+        except ObjectDoesNotExist:
+            return HttpResponseNotFound('<h1>Страница не найдена, проверьте адрес :(</h1>')
+
+    def change_active_status(self):
+
+        if self.is_active:
+            self.is_active = False
+            self.save()
+            return 'profile_url'
+        else:
+            self.is_active = True
+            self.save()
+            return 'hidden_urls_url'
+
 
 class UrlClick(models.Model):
-    related_url=models.ForeignKey(ShortenedUrl, on_delete=models.PROTECT, verbose_name="Соответствующая ссылка",
-                                  related_name='clicks')
-    click_date=models.DateField(auto_now_add=True, null=True)
+    related_url = models.ForeignKey(ShortenedUrl, on_delete=models.PROTECT, verbose_name="Соответствующая ссылка",
+                                    related_name='clicks')
+    click_date = models.DateField(auto_now_add=True, null=True)
+
+    def __init__(self, related_url=None, *args):
+        super(UrlClick, self).__init__(*args)
+        if isinstance(related_url, ShortenedUrl):
+            self.related_url = related_url
+            self.save()
+
+    @staticmethod
+    def get_statistic_for_url(url_for_stat):
+        all_clicks = UrlClick.objects.filter(related_url=ShortenedUrl(pk=url_for_stat)).count()
+        delta = timedelta(days=1)
+        today = date.today()
+        list_of_30_days = [today - delta * i for i in range(30)]  # 30 предыдущих дней начиная с сегодняшнего
+        full_stat = {datestring: len(count_of_clicks) for
+                     datestring, count_of_clicks in
+                     zip(
+                         list_of_30_days,
+                         [UrlClick.objects.filter(
+                             related_url=ShortenedUrl(pk=url_for_stat),
+                             click_date=day
+                         ) for day in list_of_30_days
+                         ]
+                     )
+                     }
+        return full_stat.items()
--- UrlShortenerApp/views.py
+++ UrlShortenerApp/views.py
@@ -10,7 +10,6 @@ from .models import ShortenedUrl, UrlClick
 from UserApp.models import User
 from PetProject.settings import SITE_BASE_URL
 from django.urls import reverse_lazy
-from datetime import date, timedelta
 
 
 class CreateUrlView(FormView):
@@ -28,10 +27,7 @@ class CreateUrlView(FormView):
     def form_valid(self, form):
         """Обработка введенных в форму данных"""
         su = ShortenedUrl()
-        if isinstance(self.request.user, User):  # проверка анонимности юзера
-            su.save(self.request.user, form.cleaned_data['user_original_url'])
-        else:
-            su.save(User.objects.get(pk=1), form.cleaned_data['user_original_url'])
+        su.save(self.request.user, form.cleaned_data['user_original_url'])
         prev_url = su.new_short_url
         return HttpResponseRedirect(self.get_success_url(prev_url=prev_url))
         # return super().form_valid(form)  # базовая версия этого метода, перенаправление по адресу,
@@ -48,14 +44,8 @@ def shorted_url_handler(request):
     Вызывается каждый раз при переходе по сокращенной ссылке и подставляет
     в адресную строку исходную ссылку
     """
-    request_url = request.get_full_path()
-    final_url = SITE_BASE_URL + request_url[1:]
-    # return HttpResponse(final_url)
-    original_URL_fromDB = ShortenedUrl.objects.get(new_short_url=final_url)
-    u = UrlClick()
-    u.related_url = original_URL_fromDB
-    u.save()
-    # return HttpResponseRedirect(original_URL_fromDB.original_url,status=404)
+    original_URL_fromDB = ShortenedUrl.get_full_url_from_requested(request)
+    u = UrlClick(related_url=original_URL_fromDB)
     return redirect(original_URL_fromDB.original_url)
 
 
@@ -63,28 +53,11 @@ def change_url_status_view(request, url_for_delete):
     """
     Если ссылка неактивна - делает ее активной и наоборот
     """
-    if ShortenedUrl.objects.get(pk=url_for_delete).is_active:
-        ShortenedUrl.objects.filter(pk=url_for_delete).update(is_active=False)
-        return redirect('profile_url')
-    else:
-        ShortenedUrl.objects.filter(pk=url_for_delete).update(is_active=True)
-        return redirect('hidden_urls_url')
+    success_url = ShortenedUrl.objects.get(pk=url_for_delete).change_active_status()
+    return redirect(success_url)
 
 
 def statistic_about_url(request, url_for_stat):
-    all_clicks = UrlClick.objects.filter(related_url=ShortenedUrl(pk=url_for_stat)).count()
-    delta = timedelta(days=1)
-    today = date.today()
-    list_of_30_days = [today - delta * i for i in range(30)]  # 30 предыдущих дней начиная с сегодняшнего
-    full_stat = {datestring: len(count_of_clicks) for
-                 datestring, count_of_clicks in
-                 zip(
-                     list_of_30_days,
-                     [UrlClick.objects.filter(
-                         related_url=ShortenedUrl(pk=url_for_stat),
-                         click_date=day
-                     ) for day in list_of_30_days
-                     ]
-                 )
-                 }
-    return render(request, 'UrlShortenerApp/statistic.html', {'full_stat': full_stat.items()})
+    """Получение статистики для ссылки за последние 30 дней"""
+    statistic = UrlClick.get_statistic_for_url(url_for_stat=url_for_stat)
+    return render(request, 'UrlShortenerApp/statistic.html', {'full_stat': statistic})
