b5127555e71fb24d26cd8545c552776ef547b5fd
DONDJ <artem.alex.alekseev@mail.ru>
2021-02-14 17:27:40 +0300

    add login requirement for /user/profile/, /user/logout/, /user/hidden/, /urls/delete_url/, /urls/stat

--- UserApp/urls.py
+++ UserApp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import *
 from UrlShortenerApp.urls import urlpatterns
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('profile', ProfileView.as_view(), name='profile_url'),
+    path('profile/', login_required(ProfileView.as_view(), login_url='/user/login/'), name='profile_url'),
     path('login/', UserLoginView.as_view(), name='login_url'),
     path('registration/', UserCreateView.as_view(), name='registration_url'),
-    path('logout/', logout_view, name='logout_url'),
-    path('hidden/', HiddenUrlsView.as_view(), name='hidden_urls_url'),
+    path('logout/', login_required(logout_view, login_url='/user/login/'), name='logout_url'),
+    path('hidden/', login_required(HiddenUrlsView.as_view(), login_url='/user/login/'), name='hidden_urls_url'),
 ]
